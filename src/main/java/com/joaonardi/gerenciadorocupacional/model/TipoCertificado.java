6a93ee3dab2e69b212a7dc06cf17eab13ab54755
Joao Vitor Nardi <joaonardiuitec@gmail.com>
2025-06-22 10:33:34 -0400

    add models para tipos

---
+++ src/main/java/com/joaonardi/gerenciadorocupacional/model/TipoCertificado.java
@@ -0,0 +1,47 @@
+package com.joaonardi.gerenciadorocupacional.model;
+
+import lombok.Getter;
+
+@Getter
+public class TipoCertificado {
+    private Integer id;
+    private String nome;
+    private Integer periodicidade;
+
+
+    public static final class TipoCertificadoBuilder {
+        private Integer id;
+        private String nome;
+        private Integer periodicidade;
+
+        private TipoCertificadoBuilder() {
+        }
+
+        public static TipoCertificadoBuilder builder() {
+            return new TipoCertificadoBuilder();
+        }
+
+        public TipoCertificadoBuilder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public TipoCertificadoBuilder nome(String nome) {
+            this.nome = nome;
+            return this;
+        }
+
+        public TipoCertificadoBuilder periodicidade(Integer periodicidade) {
+            this.periodicidade = periodicidade;
+            return this;
+        }
+
+        public TipoCertificado build() {
+            TipoCertificado tipoCertificado = new TipoCertificado();
+            tipoCertificado.id = this.id;
+            tipoCertificado.periodicidade = this.periodicidade;
+            tipoCertificado.nome = this.nome;
+            return tipoCertificado;
+        }
+    }
+}
---
+++ src/main/java/com/joaonardi/gerenciadorocupacional/model/TipoExame.java
@@ -0,0 +1,48 @@
+package com.joaonardi.gerenciadorocupacional.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Getter
+public class TipoExame {
+    private Integer id;
+    private String nome;
+    private Integer periodicidade;
+
+
+    public static final class TipoExameBuilder {
+        private Integer id;
+        private String nome;
+        private Integer periodicidade;
+
+        private TipoExameBuilder() {
+        }
+
+        public static TipoExameBuilder builder() {
+            return new TipoExameBuilder();
+        }
+
+        public TipoExameBuilder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public TipoExameBuilder nome(String nome) {
+            this.nome = nome;
+            return this;
+        }
+
+        public TipoExameBuilder periodicidade(Integer periodicidade) {
+            this.periodicidade = periodicidade;
+            return this;
+        }
+
+        public TipoExame build() {
+            TipoExame tipoExame = new TipoExame();
+            tipoExame.id = this.id;
+            tipoExame.nome = this.nome;
+            tipoExame.periodicidade = this.periodicidade;
+            return tipoExame;
+        }
+    }
+}
