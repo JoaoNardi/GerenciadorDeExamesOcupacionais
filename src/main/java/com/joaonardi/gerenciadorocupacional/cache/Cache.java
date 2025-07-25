786ae1cc49babbd349950823d0a17e15f46fe03e
Joao Vitor Nardi <joaonardiuitec@gmail.com>
2025-07-25 10:56:20 -0400

    Cache Geral Desativado por enquanto

--- src/main/java/com/joaonardi/gerenciadorocupacional/cache/Cache.java
+++ src/main/java/com/joaonardi/gerenciadorocupacional/cache/Cache.java
@@ -3,11 +3,11 @@ package com.joaonardi.gerenciadorocupacional.cache;
 public class Cache {
 
     public static void recarregarTudo(){
-        CertificadoCache.carregarCertificados();
-        ExameCache.carregarExames();
-        FuncionarioCache.carregarFuncionarios();
-        SetorCache.carregarSetores();
-        TipoCertificadoCache.carregarTiposCertificados();
-        TipoExameCache.carregarTiposExames();
+//        CertificadoCache.carregarCertificados();
+//        ExameCache.carregarExames();
+//        FuncionarioCache.carregarFuncionarios();
+//        SetorCache.carregarSetores();
+//        TipoCertificadoCache.carregarTiposCertificados();
+//        TipoExameCache.carregarTiposExames();
         }
 }
