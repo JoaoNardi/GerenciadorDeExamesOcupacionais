953e3362e2c613ceafdf1f6e0a654a4727e4da31
Joao Vitor Nardi <joaonardiuitec@gmail.com>
2025-07-25 10:58:27 -0400

    listagem por vigencia dos exames

--- src/main/java/com/joaonardi/gerenciadorocupacional/service/ExameService.java
+++ src/main/java/com/joaonardi/gerenciadorocupacional/service/ExameService.java
@@ -1,26 +1,47 @@
 package com.joaonardi.gerenciadorocupacional.service;
 
+import com.joaonardi.gerenciadorocupacional.cache.ExameCache;
 import com.joaonardi.gerenciadorocupacional.dao.ExameDAO;
 import com.joaonardi.gerenciadorocupacional.model.Exame;
+import com.joaonardi.gerenciadorocupacional.model.Periodicidade;
 import com.joaonardi.gerenciadorocupacional.model.TipoExame;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExameService {
     private ExameDAO exameDAO = new ExameDAO();
 
-    public void lancarExame(Exame exame){
+    public void lancarExame(Exame exame) {
         exameDAO.cadastrarExame(exame);
     }
 
-    public LocalDate calcularValidadeExame(LocalDate emissaoExame, TipoExame tipoExame){
-        LocalDate dataValidade = emissaoExame.plusDays(tipoExame.getPeriodicidade());
+    public LocalDate calcularValidadeExame(LocalDate emissaoExame, TipoExame tipoExame) {
+        LocalDate dataValidade = emissaoExame.plusMonths(tipoExame.getPeriodicidade());
         return dataValidade;
     }
 
-    public ObservableList<Exame> listarExames(){
-        return exameDAO.listarExames();
+    public ObservableList<Exame> listarExames() {
+        ExameCache.carregarExamesVigentes();
+        return ExameCache.todosExames;
+    }
+
+    public ObservableList<Exame> listarExamePorVencimento(int diasVencimento) {
+
+        List<Exame> list = ExameCache.todosExames;
+        list = list.stream()
+                .filter(f-> {
+                    LocalDate hoje = LocalDate.now();
+                    LocalDate validade = f.getDataValidade();
+                    return !validade.isAfter(hoje.plusMonths(diasVencimento));
+
+                })
+                .collect(Collectors.toList());
+
+        return FXCollections.observableArrayList(list);
     }
 
 }
