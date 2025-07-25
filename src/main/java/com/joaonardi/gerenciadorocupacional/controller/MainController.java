dcd56a1aef402acd393c780073da23ca1d1775cd
Joao Vitor Nardi <joaonardiuitec@gmail.com>
2025-07-25 10:58:06 -0400

    Duas Tabelas, uma geral parcialmente concluida, e a de vencimentos que comecou a ser construida agora

--- src/main/java/com/joaonardi/gerenciadorocupacional/controller/MainController.java
+++ src/main/java/com/joaonardi/gerenciadorocupacional/controller/MainController.java
@@ -1,9 +1,7 @@
 package com.joaonardi.gerenciadorocupacional.controller;
 
-import com.joaonardi.gerenciadorocupacional.dao.SetorDAO;
 import com.joaonardi.gerenciadorocupacional.model.Exame;
 import com.joaonardi.gerenciadorocupacional.model.Funcionario;
-import com.joaonardi.gerenciadorocupacional.model.Setor;
 import com.joaonardi.gerenciadorocupacional.service.ExameService;
 import com.joaonardi.gerenciadorocupacional.service.FuncionarioService;
 import com.joaonardi.gerenciadorocupacional.util.Janela;
@@ -14,6 +12,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -21,17 +20,17 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.time.format.DateTimeFormatter;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class MainController {
+    //tabela Principal Geral
     public TableView<Funcionario> tabelaPrincipal;
-    public TableColumn<Funcionario, String> colunaFuncionario;
-    public TableColumn<Funcionario, String> colunaIdade;
-    public TableColumn<Funcionario, String> colunaSetor;
+    public TableColumn<Funcionario, String> colunaFuncionarioGeral;
+    public TableColumn<Funcionario, String> colunaIdadeGeral;
+    public TableColumn<Funcionario, String> colunaSetorGeral;
     public TableColumn<Funcionario, String> colunaAniversario;
     public TableColumn<Funcionario, String> colunaStatusGeral;
-    public TableColumn<Funcionario, Button> colunaAcoes;
+    public TableColumn<Funcionario, Button> colunaAcoesGeral;
+    //botoes
     public Button btnVencidos;
     public Button btnVencemSemana;
     public Button btnVencemMes;
@@ -40,37 +39,64 @@ public class MainController {
     public Label labelTodos;
     public Label labelVencemMes;
     public Label labelVencemSemana;
+    public Label labelVencemSemestre;
     public Label labelVencidos;
+    //tabela Secundaria
+    public TableView<Exame> tabelaVencimentos;
+    public TableColumn<Exame, String> colunaFuncionarioVencimentos;
+    public TableColumn<Exame, Integer> colunaIdadeVencimentos;
+    public TableColumn<Exame, String> colunaSetorVencimentos;
+    public TableColumn<Exame, String> colunaStatusVencimentos;
+    public TableColumn<Exame, Node> colunaAcoesVencimentos;
+
     Janela janela = new Janela();
 
     ObservableList<Funcionario> funcionariosList = FXCollections.observableArrayList();
     FuncionarioService funcionarioService = new FuncionarioService();
     ExameService exameService = new ExameService();
     ObservableList<Exame> listaDeExames = exameService.listarExames();
+    int diasVencimento = 0;
 
     @FXML
     private void initialize() throws Exception {
-        labelTodos.setText(String.valueOf(funcionariosList.size()));
         recarregarListaFuncionarios();
         SetorCache.carregarSetores();
-        colunaFuncionario.setCellValueFactory(new PropertyValueFactory<>("nome"));
-        colunaIdade.setCellValueFactory(funcionarioStringCellDataFeatures -> {
-            Funcionario f = funcionarioStringCellDataFeatures.getValue();
-            int idade = funcionarioService.calcularIdade(f.getDataNascimento());
-            return new ReadOnlyObjectWrapper<>(idade).asString();
-        });
-        colunaSetor.setCellValueFactory(f -> {
-            String areaSetor = SetorCache.getSetorMapeado(f.getValue().getSetor());
-            return new SimpleStringProperty(areaSetor);
-        });
-        colunaAniversario.setCellValueFactory(funcionarioStringCellDataFeatures -> {
-            Funcionario f = funcionarioStringCellDataFeatures.getValue();
-            String dataAniversario = f.getDataNascimento().format(DateTimeFormatter.ofPattern("dd/MM"));
-            return new ReadOnlyObjectWrapper<>(dataAniversario);
-        });
-        tabelaPrincipal.setItems(funcionariosList);
+        setTabelaPrincipal();
+        setTabelaSecundaria();
+        labelTodos.setText(String.valueOf(funcionariosList.size()));
+        labelVencidos.setText(String.valueOf(exameService.listarExamePorVencimento(0).size()));
+        labelVencemSemana.setText(String.valueOf(exameService.listarExamePorVencimento(7).size()));
+        labelVencemMes.setText(String.valueOf(exameService.listarExamePorVencimento(30).size()));
+        labelVencemSemestre.setText(String.valueOf(exameService.listarExamePorVencimento(182).size()));
+    }
+
+    private void setTabelaPrincipal(){
+        if (!funcionariosList.isEmpty()) {
+            colunaFuncionarioGeral.setCellValueFactory(new PropertyValueFactory<>("nome"));
+            colunaIdadeGeral.setCellValueFactory(funcionarioStringCellDataFeatures -> {
+                Funcionario f = funcionarioStringCellDataFeatures.getValue();
+                int idade = funcionarioService.calcularIdade(f.getDataNascimento());
+                return new ReadOnlyObjectWrapper<>(idade).asString();
+            });
+            colunaSetorGeral.setCellValueFactory(f -> {
+                String areaSetor = SetorCache.getSetorMapeado(f.getValue().getSetor());
+                return new SimpleStringProperty(areaSetor);
+            });
+            colunaAniversario.setCellValueFactory(funcionarioStringCellDataFeatures -> {
+                Funcionario f = funcionarioStringCellDataFeatures.getValue();
+                String dataAniversario = f.getDataNascimento().format(DateTimeFormatter.ofPattern("dd/MM"));
+                return new ReadOnlyObjectWrapper<>(dataAniversario);
+            });
+            tabelaPrincipal.setItems(funcionariosList);
+        }
+    }
+
+    private void setTabelaSecundaria(){
+        colunaFuncionarioVencimentos.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        tabelaVencimentos.setItems(listaDeExames);
 
     }
+
     private void recarregarListaFuncionarios(){
         try {
             funcionariosList = funcionarioService.carregarFuncionarios(true);
@@ -119,5 +145,24 @@ public class MainController {
     }
 
     public void handleGerenciarExames(ActionEvent event) {
+    }
+
+    public void handleBtnVencidos(ActionEvent event) {
+        diasVencimento = 0;
+    }
+
+    public void handleBtnSemana(ActionEvent event) {
+        diasVencimento = 7;
+    }
+
+    public void handleBtnMes(ActionEvent event) {
+        diasVencimento = 30;
+    }
+
+    public void handleBtnSemestre(ActionEvent event) {
+        diasVencimento = 182;
+    }
+
+    public void handleBtnTodos(ActionEvent event) {
     }
 }
