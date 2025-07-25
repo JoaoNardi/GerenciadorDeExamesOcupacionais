71a8e84ce608ab6b4520200e6cfea652b2dd9cf4
Joao Vitor Nardi <joaonardiuitec@gmail.com>
2025-07-25 07:56:25 -0400

    Metodo Cache para memoria de exames

--- src/main/java/com/joaonardi/gerenciadorocupacional/cache/ExameCache.java
+++ src/main/java/com/joaonardi/gerenciadorocupacional/cache/ExameCache.java
@@ -1,7 +1,13 @@
 package com.joaonardi.gerenciadorocupacional.cache;
 
-public class ExameCache {
-    public static void carregarExames() {
+import com.joaonardi.gerenciadorocupacional.dao.ExameDAO;
+import com.joaonardi.gerenciadorocupacional.model.Exame;
+import javafx.collections.ObservableList;
 
+public class ExameCache {
+    private static ExameDAO exameDAO = new ExameDAO();
+    public static ObservableList<Exame> todosExames;
+    public static void carregarExames() {
+        todosExames = exameDAO.listarExames();
     }
 }
