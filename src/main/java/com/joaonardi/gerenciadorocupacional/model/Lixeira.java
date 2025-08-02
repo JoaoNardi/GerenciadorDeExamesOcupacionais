c6fc5e4c2a7aea0a3b607715fe6d2f5e18eefb2b
Joao Vitor Nardi <joaonardiuitec@gmail.com>
2025-08-02 09:24:44 -0400

    ideia para o futuro

---
+++ src/main/java/com/joaonardi/gerenciadorocupacional/model/Lixeira.java
@@ -0,0 +1,6 @@
+package com.joaonardi.gerenciadorocupacional.model;
+
+public class Lixeira {
+    // Sistema de delete seguro
+    // um Item apos ser colocado na lixeira sera excluido automaticamente em 30 dias
+}
