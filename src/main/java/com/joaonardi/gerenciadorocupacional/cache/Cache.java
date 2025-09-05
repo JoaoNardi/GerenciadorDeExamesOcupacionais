1fd02b7ab4759d35e2d4f78769a39856449a8cb4
Joao Nardi <joaovitornard@gmail.com>
2025-09-05 10:45:56 -0400

    Refazer a memoria do sistema inteiro

--- src/main/java/com/joaonardi/gerenciadorocupacional/cache/Cache.java
+++ src/main/java/com/joaonardi/gerenciadorocupacional/cache/Cache.java
@@ -3,6 +3,7 @@ package com.joaonardi.gerenciadorocupacional.cache;
 public class Cache {
 
     public static void recarregarTudo(){
+        // TODO: Melhorar todo sistema de cache, padronizar e aplicar em todo o projeto
 //        CertificadoCache.carregarCertificados();
 //        ExameCache.carregarExames();
 //        FuncionarioCache.carregarFuncionarios();
